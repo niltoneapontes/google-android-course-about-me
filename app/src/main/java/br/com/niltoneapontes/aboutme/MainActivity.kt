c76566a6478b3f82fefcdf01c604879992cdad04
Nilton <niltoneapontes@gmail.com>
2023-05-31 10:03:24 -0300

    feat: implementing databinding

--- app/src/main/java/br/com/niltoneapontes/aboutme/MainActivity.kt
+++ app/src/main/java/br/com/niltoneapontes/aboutme/MainActivity.kt
@@ -8,24 +8,27 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import br.com.niltoneapontes.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.button).setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.button.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val tvNickname = findViewById<TextView>(R.id.nickname_text)
-
-        tvNickname.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        tvNickname.visibility = View.VISIBLE
+        binding.apply {
+            nicknameText.text = nicknameEdit.text
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameEdit.visibility = View.VISIBLE
+        }
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
